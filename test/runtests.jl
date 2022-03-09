a658763f98a08fe6bc27f3c372b3f9f62fc3fce8
Brian Jackson <bjack205@gmail.com>
2022-03-09 11:54:19 -0500

    Use new autograder

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,36 @@
-module Q1
-    using NBInclude
-    @nbinclude(joinpath(@__DIR__, "..", "src", "Q1.ipynb"))
+const NUMQUESTIONS = 2
+
+questions_to_grade = collect(1:NUMQUESTIONS)
+if !isempty(ARGS)
+    questions_to_grade = Int[]
+    for arg in ARGS
+        question = tryparse(Int, arg)
+        if !isnothing(question) && (0 < question <= NUMQUESTIONS)
+            push!(questions_to_grade, question)
+        end
+    end
+    if isempty(questions_to_grade)
+        @warn "Couldn't parse any of the input arguments as question numbers. Enter the question to grade, separated by spaced."
+    end
+    sort!(questions_to_grade)
 end
-module Q2
-    using NBInclude
-    @nbinclude(joinpath(@__DIR__, "..", "src", "Q2.ipynb"))
+
+
+# Create a module for each question
+for i in 1:NUMQUESTIONS 
+@eval module $(Symbol("Q" * string(i)))
+include("autograder.jl")
+getname() = string(split(string(@__MODULE__), ".")[end])
+grade() = Autograder.gradequestion(getname()) 
+checktestsets(solutiondir=joinpath(@__DIR__, "..")) = Autograder.checktestsets(getname(), solutiondir)
+end
+end
+
+solutiondir = joinpath(@__DIR__, "..")
+
+# Grade all of the questions
+results = map(questions_to_grade) do question 
+    mod = modules[question]
+    mod.checktestsets(solutiondir)
+    mod.grade()[1]
 end
